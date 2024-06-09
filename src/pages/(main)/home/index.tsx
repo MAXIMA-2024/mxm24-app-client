0cfe6940fff8bd871b8aa51cbe56993644b86b37
AlbertTech23 <alberttirtokusumo@gmail.com>
2024-06-09 16:14:39 +0700

    feat: home desktop done

--- src/pages/(main)/home/index.tsx
+++ src/pages/(main)/home/index.tsx
@@ -1,4 +1,19 @@
-import { Stack, Text } from "@chakra-ui/react";
+import { Stack, Text, Image, Button } from "@chakra-ui/react";
+
+const CardData = [
+  {
+    title: "Challenge",
+    icon: "/icons/challenge-icon.png",
+  },
+  {
+    title: "Handbook",
+    icon: "/icons/handbook-icon.png",
+  },
+  {
+    title: "Twibbon",
+    icon: "/icons/twibbon-icon.png",
+  },
+];
 
 const Home = () => {
   return (
@@ -11,7 +26,36 @@ const Home = () => {
       bgRepeat={"no-repeat"}
       bgPosition={"center"}
       minH={"100vh"}
+      justifyContent={"center"}
+      alignItems={"center"}
     >
+      <Stack direction={"row"} gap={5} mt={"25rem"}>
+        {CardData.map((card, index) => (
+          <Button
+            flexDirection={"column"}
+            variant={"ghost"}
+            key={index}
+            bgColor={"card.primary"}
+            p={5}
+            rounded={"xl"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            w={"14rem"}
+            h={"9rem"}
+            gap={4}
+            _hover={{
+              bgColor: "text.tertiary",
+              transform: "scale(0.95)",
+              transition: "all 0.2s ease-in-out",
+            }}
+          >
+            <Image src={card.icon} w={"2.5rem"} h={"2.5rem"} />
+            <Text fontFamily={"Lexend"} fontSize={"1.3rem"} fontWeight={400}>
+              {card.title}
+            </Text>
+          </Button>
+        ))}
+      </Stack>
       {/* <Text>HoME</Text> */}
     </Stack>
   );
