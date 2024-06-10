d576d1afa910fe79ccc399c362a3516a713420f1
AlbertTech23 <alberttirtokusumo@gmail.com>
2024-06-10 17:19:20 +0700

    fix: fontSize for Login and Dashboard

--- src/pages/(main)/dashboard/index.tsx
+++ src/pages/(main)/dashboard/index.tsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
     <>
       <Stack
         alignItems={"center"}
-        justifyContent={"space-around"}
+        justifyContent={"center"}
         minH={"100vh"}
         bgImage={{
           base: "/bg/welcome-page-mobile.png",
@@ -15,18 +15,24 @@ const Dashboard = () => {
         bgRepeat={"no-repeat"}
         bgPosition={"center"}
       >
-        <Stack align={"center"} color={"text.primary"} fontWeight={"700"}>
+        <Stack
+          align={"center"}
+          color={"text.primary"}
+          fontWeight={"700"}
+          pb={{ base: "15rem", md: "22.5rem", lg: "20rem", xl: "25rem" }}
+          gap={{ base: 0, lg: 2 }}
+        >
           <Heading
             fontFamily={"Luthier"}
-            fontSize={{ base: "1.75rem", md: "4rem", lg: "6rem", xl: "7.5rem" }}
-            // size={{ base: "xl", sm: "4xl" }}
+            fontSize={{ base: "2rem", md: "4rem", lg: "6rem", xl: "7.5rem" }}
+            textShadow={"0 0 5rem #000000"}
           >
             Selamat Datang
           </Heading>
           <Heading
             fontFamily={"Luthier"}
-            fontSize={{ base: "4rem", sm: "7.5rem" }}
-            // size={"4xl"}
+            fontSize={{ base: "2rem", md: "4rem", lg: "6rem", xl: "7.5rem" }}
+            textShadow={"0 0 5rem #000000"}
           >
             Maximers!
           </Heading>
