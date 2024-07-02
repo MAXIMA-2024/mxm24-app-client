434aedb3fed9d379c71b87ac2c07266f80c115f5
Gian Avila <avila.gian111@gmail.com>
2024-07-02 17:13:44 +0700

    feat: add maxi and xima, change button direction in about us page

--- src/pages/(main)/aboutus/index.tsx
+++ src/pages/(main)/aboutus/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Stack, Text, Button } from "@chakra-ui/react";
+import { Stack, Text, Button, Image, Show } from "@chakra-ui/react";
 
 const ButtonData = [
   {
@@ -35,25 +35,47 @@ const AboutUs = () => {
         gap={5}
         alignItems={"center"}
         mx={"20%"}
-        mt={"5%"}
+        mt={{ base: "10rem", lg: "5rem", xl: "0rem" }}
+        position={"relative"}
       >
+        <Image
+          src="/bg/maxi.svg"
+          alt="test aja"
+          h={{ base: "4rem", md: "7rem" }}
+          position={"absolute"}
+          left={{ base: "0.8rem", md: "1.5rem", lg: "2rem", xl: "2rem" }}
+          top={{ base: "4.75rem", md: "0.45rem", lg: "0.5rem", xl: "2.7rem" }}
+        ></Image>
+
+        <Image
+          src="/bg/xima.svg"
+          alt="test aja"
+          h={{ base: "4rem", md: "7rem" }}
+          position={"absolute"}
+          left={{ base: "13.5rem", md: "32.5rem", lg: "41.5rem", xl: "60rem" }}
+          top={{ base: "4.55rem", md: "0.15rem", lg: "0.1rem", xl: "2.4rem" }}
+        ></Image>
+
         <Text
-          fontSize={{ base: "3rem", md: "3rem", lg: "3rem", xl: "6rem" }}
+          fontSize={{ base: "3rem", md: "3rem", lg: "3rem", xl: "4.5rem" }}
           fontFamily={"Luthier"}
           fontWeight={"bold"}
           color={"text.primary"}
+          mb={{ base: "2rem", md: "0rem" }}
         >
           About Us
         </Text>
 
         <Stack
           bgColor={"#FAF4E8"}
-          px={{ base: "30px", md: "40px", lg: "50px", xl: "70px" }}
+          w={{ base: "350px", md: "750px", lg: "900px", xl: "1200px" }}
+          px={{ base: "30px", md: "60px", lg: "50px", xl: "70px" }}
           py={{ base: "30px", lg: "30px", xl: "30px" }}
           borderRadius={"3xl"}
           alignItems={"center"}
           gap={5}
           boxShadow={"lg"}
+          mb={{ base: 10, lg: 0 }}
         >
           {/* ISI CONTENT START */}
           <Text
@@ -85,28 +107,29 @@ const AboutUs = () => {
           </Text>
           {/* ISI CONTENT END */}
 
-          {ButtonData.map((button, index) => (
-            <Button
-              key={index}
-              bgColor={"text.primary"}
-              size={{ base: "sm", lg: "md", xl: "lg" }}
-              h={"50px"}
-              fontFamily={"Lexend"}
-              color={"white"}
-              fontWeight={"base"}
-              borderRadius={"xl"}
-              _hover={{
-                color: "text.primary",
-                transform: "scale(0.95)",
-                transition: "all 0.2s ease-in-out",
-                bgColor: "#FAF4E8",
-                border: "1px",
-                borderColor: "text.primary",
-              }}
-            >
-              <Link to={button.link}>{button.content}</Link>
-            </Button>
-          ))}
+          <Stack direction={{ base: "column", md: "row" }} gap={5}>
+            {ButtonData.map((button, index) => (
+              <Button
+                key={index}
+                bgColor={"text.primary"}
+                size={{ base: "sm", lg: "md", xl: "lg" }}
+                fontFamily={"Lexend"}
+                color={"white"}
+                fontWeight={"base"}
+                borderRadius={"xl"}
+                _hover={{
+                  color: "text.primary",
+                  transform: "scale(0.95)",
+                  transition: "all 0.2s ease-in-out",
+                  bgColor: "#FAF4E8",
+                  border: "1px",
+                  borderColor: "text.primary",
+                }}
+              >
+                <Link to={button.link}>{button.content}</Link>
+              </Button>
+            ))}
+          </Stack>
         </Stack>
       </Stack>
     </Stack>
--- src/pages/(main)/faq/index.tsx
+++ src/pages/(main)/faq/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Stack, Text, Button } from "@chakra-ui/react";
+import { Stack, Text, Button, Image } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
 const ButtonData = [
@@ -87,18 +87,49 @@ const Faq = () => {
         alignItems={"center"}
         mx={"20%"}
         mt={"5%"}
+        position={"relative"}
       >
+        <Image
+          src="/bg/maxi.svg"
+          alt="test aja"
+          h={{ base: "4rem", md: "7rem" }}
+          position={"absolute"}
+          left={{ base: "0.8rem", md: "1.5rem", lg: "2rem", xl: "2rem" }}
+          top={{
+            base: "3.72rem",
+            md: "0.45rem",
+            lg: "0.5rem",
+            xl: "4.96rem",
+          }}
+        ></Image>
+
+        <Image
+          src="/bg/xima.svg"
+          alt="test aja"
+          h={{ base: "4rem", md: "7rem" }}
+          position={"absolute"}
+          left={{ base: "13.5rem", md: "29.5rem", lg: "41.5rem", xl: "60rem" }}
+          top={{ base: "3.55rem", md: "0.15rem", lg: "0.15rem", xl: "4.65rem" }}
+        ></Image>
+
         <Text
           fontSize={{ base: "3rem", md: "3rem", lg: "3rem", xl: "6rem" }}
           fontFamily={"Luthier"}
           fontWeight={"bold"}
           color={"text.primary"}
+          mb={{ base: "1rem", md: "0" }}
         >
           FAQ
         </Text>
 
         <Stack
           bgColor={"#FAF4E8"}
+          w={{
+            base: "350px",
+            md: "700px",
+            lg: "900px",
+            xl: "1200px",
+          }}
           px={{ base: "30px", md: "40px", lg: "50px", xl: "70px" }}
           py={{ base: "30px", lg: "30px", xl: "30px" }}
           borderRadius={"3xl"}
@@ -112,6 +143,7 @@ const Faq = () => {
             gap={5}
             p={"15px"}
             borderRadius={"full"}
+            mt={"1rem"}
           >
             {ButtonData.map((button) => (
               <Button
