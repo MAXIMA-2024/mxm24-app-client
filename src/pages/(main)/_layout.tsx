1f301aa464f430846aa107d8b29f748feb50ae7a
Gian Avila <avila.gian111@gmail.com>
2024-07-25 22:34:41 +0700

    feat: add login with sso button for users who are not logged in

--- src/pages/(main)/_layout.tsx
+++ src/pages/(main)/_layout.tsx
@@ -10,6 +10,14 @@ import {
   Menu,
   MenuButton,
   MenuList,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
   // Drawer,
   // DrawerBody,
   // DrawerFooter,
@@ -25,6 +33,7 @@ import { IoLogOutOutline } from "react-icons/io5";
 import { FaRegEdit } from "react-icons/fa";
 import { Link, Outlet } from "react-router-dom";
 import Sidebar from "@/components/Sidebar/Sidebar";
+import React from "react";
 
 const NavBarData = [
   {
@@ -55,6 +64,22 @@ const NavBarData = [
 ];
 
 const MainLayoutDesktop = () => {
+  const isLogin = false;
+
+  const user = {
+    status: isLogin ? "authenticated" : "unaunthenticated",
+    name: "Jonathan",
+  };
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const OverlayOne = () => (
+    <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px)" />
+  );
+
+  const [overlay, setOverlay] = React.useState(<OverlayOne />);
+
+  const paddingRight = user.status !== "authenticated" ? { lg: 4, xl: 6 } : {};
+
   return (
     <>
       <Stack
@@ -69,19 +94,20 @@ const MainLayoutDesktop = () => {
       >
         <Stack
           bgColor={"rgba(255, 255, 255, 0.85)"}
-          p={5}
-          py={{ lg: 0, xl: 8 }}
-          px={{ lg: 5, xl: 12 }}
+          // p={5}
+          py={{ lg: 4, xl: 4 }}
+          pl={{ lg: 5, xl: 2 }}
+          pr={paddingRight}
           direction={"row"}
           alignItems={"center"}
-          justifyContent={"space-between"}
+          justifyContent={"space-around"}
           backdropFilter={"blur(10px)"}
           rounded={"full"}
           // roundedTopRight={"xl"}
           // roundedBottomRight={"xl"}
           gap={{ lg: 3, xl: 12 }}
           // px={{ lg: 22, xl: 20 }}
-          h={"5rem"}
+          // h={"5rem"}
           // w={"50%"}
           // w={"70rem"}
           // w={"50%"}
@@ -138,116 +164,192 @@ const MainLayoutDesktop = () => {
             ))}
           </Stack>
           {/* Right Side */}
-          <Menu>
-            <MenuButton>
-              <Button
-                variant={"ghost"}
-                w={"full"}
-                justifyContent={"start"}
-                // p={[0, 0, 0, 2]}
-                // mt={[0, 0, "0.75rem", "1rem"]}
-                cursor={"default"}
-                disabled={true}
-                _hover={{
-                  pointerEvents: "none",
-                  draggable: "none",
-                }}
-              >
-                <Stack
-                  direction={"row"}
-                  justifyContent={"center"}
-                  alignItems={"center"}
-                  // pr={{ lg: 12, xl: 20 }}
-                >
-                  <Avatar
-                    bg={"#44002B"}
-                    size={"md"}
-                    _hover={{ transform: "scale(0.95)" }}
-                    transition={"all 0.2s ease-in-out"}
-                  >
-                    <AvatarBadge
-                      boxSize={{ lg: "0.75rem", xl: "1.25rem" }}
-                      bg="green.500"
-                    />
-                  </Avatar>
-                  <Hide below="xl">
-                    <Stack
-                      justifyContent={"center"}
-                      alignItems={"center"}
-                      textAlign={"left"}
-                      gap={0}
-                      color={"text.primary"}
-                      fontFamily={"Lexend"}
-                      fontWeight={500}
-                      mx={{ lg: 0, xl: 2.5 }}
-                      noOfLines={2}
-                      isTruncated={true}
-                      textOverflow={"ellipsis"}
-                      // letterSpacing={"0.1rem"}
-                      // textShadow={"0 0 1rem #000000"}
-                    >
-                      <Text fontSize={{ lg: "x-small", xl: "smaller" }}>
-                        Halo,
-                      </Text>
-                      <Text
-                        fontWeight={600}
-                        fontSize={{ lg: "smaller", xl: "large" }}
-                      >
-                        Jonathan
-                      </Text>
-                    </Stack>
-                  </Hide>
-                </Stack>
-              </Button>
-            </MenuButton>
-            <MenuList px={2} shadow={"lg"} borderRadius={"2xl"}>
-              <Link to={"/auth/profile"}>
+
+          {user.status === "authenticated" && (
+            <Menu>
+              <MenuButton>
                 <Button
-                  // bg={"status.error"}
+                  variant={"ghost"}
+                  w={"full"}
+                  justifyContent={"start"}
+                  // p={[0, 0, 0, 2]}
+                  // mt={[0, 0, "0.75rem", "1rem"]}
+                  cursor={"default"}
+                  disabled={true}
+                  _hover={{
+                    pointerEvents: "none",
+                    draggable: "none",
+                  }}
+                >
+                  <Stack
+                    direction={"row"}
+                    // justifyContent={"end"}
+                    // alignItems={"end"}
+                    // pl={{ lg: 12, xl: 20 }}
+                  >
+                    <Avatar
+                      bg={"#44002B"}
+                      size={"md"}
+                      _hover={{ transform: "scale(0.95)" }}
+                      transition={"all 0.2s ease-in-out"}
+                    >
+                      <AvatarBadge
+                        boxSize={{ lg: "0.75rem", xl: "1.25rem" }}
+                        bg="green.500"
+                      />
+                    </Avatar>
+
+                    <Hide below="xl">
+                      <Stack
+                        justifyContent={"center"}
+                        alignItems={"center"}
+                        textAlign={"left"}
+                        gap={0}
+                        color={"text.primary"}
+                        fontFamily={"Lexend"}
+                        fontWeight={500}
+                        mx={{ lg: 0, xl: 2.5 }}
+                        noOfLines={2}
+                        isTruncated={true}
+                        textOverflow={"ellipsis"}
+                        // letterSpacing={"0.1rem"}
+                        // textShadow={"0 0 1rem #000000"}
+                      >
+                        <Stack p={1} gap={0}>
+                          <Text fontSize={{ lg: "x-small", xl: "smaller" }}>
+                            Halo,
+                          </Text>
+                          <Text
+                            fontWeight={600}
+                            fontSize={{ lg: "smaller", xl: "large" }}
+                          >
+                            Jonathan
+                          </Text>
+                        </Stack>
+                      </Stack>
+                    </Hide>
+                  </Stack>
+                </Button>
+              </MenuButton>
+
+              <MenuList px={2} shadow={"lg"} borderRadius={"2xl"}>
+                <Link to={"/auth/profile"}>
+                  <Button
+                    // bg={"status.error"}
+                    p={2}
+                    py={0}
+                    my={1}
+                    mb={2}
+                    fontSize={["0.8rem", "0.8rem", "0.85rem", "1rem"]}
+                    borderRadius={"xl"}
+                    color={"black"}
+                    fontWeight={"medium"}
+                    gap={2}
+                    w={"full"}
+                    justifyContent={"center"}
+                    // onClick={auth.logout}
+                  >
+                    <FaRegEdit
+                      color="black"
+                      fontSize={"1rem"}
+                      fontWeight={"bold"}
+                    />
+                    Edit Profile
+                  </Button>
+                </Link>
+                <Button
+                  bg={"status.error"}
                   p={2}
                   py={0}
                   my={1}
-                  mb={2}
                   fontSize={["0.8rem", "0.8rem", "0.85rem", "1rem"]}
                   borderRadius={"xl"}
-                  color={"black"}
+                  color={"white"}
                   fontWeight={"medium"}
                   gap={2}
                   w={"full"}
-                  justifyContent={"center"}
+                  // justifyContent={"center"}
                   // onClick={auth.logout}
                 >
-                  <FaRegEdit
-                    color="black"
-                    fontSize={"1rem"}
+                  <IoLogOutOutline
+                    color="white"
+                    fontSize={"1.25rem"}
                     fontWeight={"bold"}
                   />
-                  Edit Profile
+                  Log Out
                 </Button>
-              </Link>
+              </MenuList>
+            </Menu>
+          )}
+
+          {user.status !== "authenticated" && (
+            <>
               <Button
-                bg={"status.error"}
-                p={2}
-                py={0}
-                my={1}
-                fontSize={["0.8rem", "0.8rem", "0.85rem", "1rem"]}
-                borderRadius={"xl"}
-                color={"white"}
-                fontWeight={"medium"}
-                gap={2}
-                w={"full"}
-                // justifyContent={"center"}
-                // onClick={auth.logout}
+                rounded={"full"}
+                bgColor={"text.primary"}
+                p={"1.5rem"}
+                shadow={"md"}
+                onClick={() => {
+                  setOverlay(<OverlayOne />);
+                  onOpen();
+                }}
               >
-                <IoLogOutOutline
-                  color="white"
-                  fontSize={"1.25rem"}
-                  fontWeight={"bold"}
-                />
-                Log Out
+                <Text color={"text.tertiary"} fontWeight={"medium"}>
+                  Login with SSO
+                </Text>
               </Button>
-            </MenuList>
-          </Menu>
+
+              <Modal
+                isOpen={isOpen}
+                onClose={onClose}
+                isCentered={true}
+                motionPreset="slideInBottom"
+              >
+                {overlay}
+                <ModalContent>
+                  {/* <ModalHeader>Modal Title</ModalHeader> */}
+                  <ModalCloseButton />
+                  <ModalBody>
+                    <Text
+                      fontFamily={"Lexend"}
+                      py={"3rem"}
+                      textAlign={"center"}
+                      fontSize={"1.6rem"}
+                    >
+                      Apakah kamu Mahasiswa/i UMN angkatan 2024?
+                    </Text>
+                  </ModalBody>
+
+                  <ModalFooter justifyContent={"center"} pb={"2rem"}>
+                    <Button
+                      as={Link}
+                      to={`https://sso.umn.ac.id/cas/login?service=${
+                        import.meta.env.VITE_FRONTEND_URL + "/auth/sso"
+                      }`}
+                      bgColor={"button.primary"}
+                      mr={3}
+                      onClick={onClose}
+                      color={"text.tertiary"}
+                      _hover={{ bgColor: "#3A0025" }}
+                    >
+                      Iya Mahasiswa 2024
+                    </Button>
+                    <Button
+                      variant="ghost"
+                      borderWidth={1}
+                      borderColor={"button.primary"}
+                      color={"text.primary"}
+                      as={Link}
+                      to={`/malpun`}
+                      onClick={onClose}
+                    >
+                      Bukan Mahasiswa 2024
+                    </Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
+            </>
+          )}
         </Stack>
       </Stack>
       <Stack>
--- src/pages/(main)/index.tsx
+++ src/pages/(main)/index.tsx
@@ -1,13 +1,35 @@
 import useAuth from "@/hooks/useAuth";
-import { Heading, Stack, Text, Button, useToast } from "@chakra-ui/react";
+import {
+  Heading,
+  Stack,
+  Text,
+  Button,
+  useToast,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "@/router";
 import { useEffect } from "react";
+import React from "react";
 
 const LandingPage = () => {
   const auth = useAuth();
   const nav = useNavigate();
   const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const OverlayOne = () => (
+    <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px)" />
+  );
+
+  const [overlay, setOverlay] = React.useState(<OverlayOne />);
 
   useEffect(() => {
     if (auth.status === "authenticated") {
@@ -37,10 +59,11 @@ const LandingPage = () => {
     <>
       <Stack
         alignItems={"center"}
-        justifyContent={
-          auth.status === "authenticated" ? "flex-start" : "space-around"
-        }
-        pt={auth.status === "authenticated" ? "10rem" : "0"}
+        gap={"15rem"}
+        // justifyContent={
+        //   auth.status === "authenticated" ? "flex-start" : "space-around"
+        // }
+        pt={auth.status === "authenticated" ? "8rem" : "0"}
         minH={"100vh"}
         bgImage={{
           base: "/bg/welcome-page-mobile.png",
@@ -60,8 +83,13 @@ const LandingPage = () => {
           {auth.status === "authenticated" ? (
             <Heading
               fontFamily={"Luthier"}
-              fontSize={{ base: "2rem", md: "4rem", lg: "5rem", xl: "6.5rem" }}
-              textShadow={"0 0 5rem #000000"}
+              fontSize={{
+                base: "2rem",
+                md: "4rem",
+                lg: "5rem",
+                xl: "5rem",
+              }}
+              // textShadow={"0 0 5rem #000000"}
             >
               Selamat Datang
             </Heading>
@@ -69,39 +97,98 @@ const LandingPage = () => {
             <Heading
               fontFamily={"Luthier"}
               fontSize={{ base: "2rem", md: "4rem", lg: "5rem", xl: "6.5rem" }}
-              textShadow={"0 0 5rem #000000"}
+              // textShadow={"0 0 5rem #000000"}
             >
               Halo! Selamat Datang
             </Heading>
           )}
           <Heading
             fontFamily={"Luthier"}
-            fontSize={{ base: "2rem", md: "4rem", lg: "6rem", xl: "7.5rem" }}
-            textShadow={"0 0 5rem #000000"}
+            fontSize={{ base: "2rem", md: "4rem", lg: "6rem", xl: "6.5rem" }}
+            // textShadow={"0 0 5rem #000000"}
           >
             Maximers!
           </Heading>
         </Stack>
-        {auth.status !== "authenticated" && (
-          <Button
-            as={Link}
-            to={`https://sso.umn.ac.id/cas/login?service=${
-              import.meta.env.VITE_FRONTEND_URL + "/auth/sso"
-            }`}
-            bgColor={"button.primary"}
-            p={8}
-            px={["3rem", "3rem", "5rem", "5rem"]}
-            variant={"ghost"}
-            transition={"0.3s"}
-            color={"text.tertiary"}
-            rounded={"xl"}
-            _hover={{ bgColor: "#3A0025" }}
-            mb={{ base: 0, md: 10 }}
-          >
-            <Text fontFamily={"Lexend"} fontWeight={"400"} fontSize={"x-large"}>
-              Login With SSO
-            </Text>
-          </Button>
+        {auth.status === "authenticated" && (
+          <>
+            <Button
+              // as={Link}
+              // to={`https://sso.umn.ac.id/cas/login?service=${
+              //   import.meta.env.VITE_FRONTEND_URL + "/auth/sso"
+              // }`}
+              bgColor={"button.primary"}
+              p={8}
+              px={["3rem", "3rem", "5rem", "5rem"]}
+              variant={"ghost"}
+              transition={"0.3s"}
+              color={"text.tertiary"}
+              rounded={"xl"}
+              _hover={{ bgColor: "#3A0025" }}
+              mb={{ base: 0, md: 10 }}
+              onClick={() => {
+                setOverlay(<OverlayOne />);
+                onOpen();
+              }}
+            >
+              <Text
+                fontFamily={"Lexend"}
+                fontWeight={"400"}
+                fontSize={"x-large"}
+              >
+                Login With SSO
+              </Text>
+            </Button>
+            <Modal
+              isOpen={isOpen}
+              onClose={onClose}
+              isCentered={true}
+              motionPreset="slideInBottom"
+            >
+              {overlay}
+              <ModalContent>
+                {/* <ModalHeader>Modal Title</ModalHeader> */}
+                <ModalCloseButton />
+                <ModalBody>
+                  <Text
+                    fontFamily={"Lexend"}
+                    py={"3rem"}
+                    textAlign={"center"}
+                    fontSize={"1.6rem"}
+                  >
+                    Apakah kamu Mahasiswa/i UMN angkatan 2024?
+                  </Text>
+                </ModalBody>
+
+                <ModalFooter justifyContent={"center"} pb={"2rem"}>
+                  <Button
+                    as={Link}
+                    to={`https://sso.umn.ac.id/cas/login?service=${
+                      import.meta.env.VITE_FRONTEND_URL + "/auth/sso"
+                    }`}
+                    bgColor={"button.primary"}
+                    mr={3}
+                    onClick={onClose}
+                    color={"text.tertiary"}
+                    _hover={{ bgColor: "#3A0025" }}
+                  >
+                    Iya Mahasiswa 2024
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    borderWidth={1}
+                    borderColor={"button.primary"}
+                    color={"text.primary"}
+                    as={Link}
+                    to={`/malpun`}
+                    onClick={onClose}
+                  >
+                    Bukan Mahasiswa 2024
+                  </Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          </>
         )}
       </Stack>
     </>
