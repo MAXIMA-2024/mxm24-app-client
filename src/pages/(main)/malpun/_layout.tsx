d8dee7590a562a593d3202c3a6ce3d5f3e5c7d43
AlbertTech23 <alberttirtokusumo@gmail.com>
2024-07-28 19:35:02 +0700

    style: differ bg for malpun or not

--- src/pages/(main)/malpun/_layout.tsx
+++ src/pages/(main)/malpun/_layout.tsx
@@ -1,17 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Stack } from "@chakra-ui/react";
 
 const MalpunLayout = () => {
+  const loct = useLocation();
+
   return (
     <Stack
       alignItems={"center"}
       justifyContent={{ base: "center", lg: "space-around" }}
       minH={"100vh"}
       minW={"100vw"}
-      bgImage={{
-        base: "/bg/malpun-bg-mobile.png",
-        lg: "/bg/malpun-bg-desktop.png",
-      }}
+      bgImage={
+        loct.pathname.endsWith("malpun")
+          ? {
+              base: "/bg/malpun-mobile.png",
+              lg: "/bg/malpun-desktop-main.png",
+            }
+          : {
+              base: "/bg/malpun-bg-mobile.png",
+              lg: "/bg/malpun-desktop-main.png",
+            }
+      }
       bgSize={"cover"}
       bgRepeat={"no-repeat"}
       bgPosition={{ base: "", md: "center" }}
