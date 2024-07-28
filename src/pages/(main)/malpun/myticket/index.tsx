b5e5c4b5880c01cf0754269c6ded526b36f5d826
AlbertTech23 <alberttirtokusumo@gmail.com>
2024-07-28 20:43:11 +0700

    feat: mobile ticket malpun done

--- src/pages/(main)/malpun/myticket/index.tsx
+++ src/pages/(main)/malpun/myticket/index.tsx
@@ -15,14 +15,16 @@ const zoomIn = keyframes`
 
 const MyTicket = () => {
   const imageRightSrc = useBreakpointValue({
-    base: "/bg/mock-ticket-malpun-mobile.png",
+    base: "/bg/bottom-ticket-malpun.png",
     lg: "/bg/right-ticket-malpun.png",
   });
 
   const imageLeftSrc = useBreakpointValue({
-    base: "/bg/mock-ticket-malpun-mobile.png",
+    base: "/bg/top-ticket-malpun.png",
     lg: "/bg/left-ticket-malpun.png",
   });
+
+  const isMobile = useBreakpointValue({ base: true, lg: false });
 
   return (
     <>
@@ -31,14 +33,28 @@ const MyTicket = () => {
         alignItems={"center"}
         position={"relative"}
         animation={`${zoomIn} 1s`}
-        direction={"row"}
+        direction={{ base: "column", lg: "row" }}
         gap={0}
         justifyContent={"center"}
         pt={{ base: "2rem", lg: "6rem" }}
       >
         {/* LEFT TICKET */}
-        <Stack w={{ base: "22rem", md: "25rem", lg: "60rem", xl: "50rem" }}>
+        <Stack
+          w={{ base: "22rem", md: "25rem", lg: "60rem", xl: "50rem" }}
+          align={"center"}
+          justify={"center"}
+          pos={"relative"}
+        >
           <Image src={imageLeftSrc}></Image>
+          {/* QR Placeholder MOBILE */}
+          {isMobile && (
+            <Image
+              src="/icons/qr-dummy.png"
+              w={"40%"}
+              pos={"absolute"}
+              mt={{ base: "2rem", lg: "0" }}
+            ></Image>
+          )}
         </Stack>
         {/* RIGHT TICKET */}
         <Stack
@@ -48,13 +64,15 @@ const MyTicket = () => {
           align={"center"}
         >
           <Image src={imageRightSrc}></Image>
-          {/* QR Placeholder */}
-          <Image
-            src="/icons/qr-dummy.png"
-            w={"60%"}
-            pos={"absolute"}
-            mr={"3.5rem"}
-          ></Image>
+          {/* QR Placeholder DESKTOP */}
+          {!isMobile && (
+            <Image
+              src="/icons/qr-dummy.png"
+              w={"60%"}
+              pos={"absolute"}
+              mr={{ base: "0rem", lg: "3.5rem" }}
+            ></Image>
+          )}
         </Stack>
       </Stack>
     </>
