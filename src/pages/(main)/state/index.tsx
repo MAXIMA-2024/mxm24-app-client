294c78317f0fb19bb1fb669be2a3ebae16680665
Opengheimer <intltcsfleader@gmail.com>
2024-07-29 11:22:00 +0700

    fix: state page init

--- src/pages/(main)/state/index.tsx
+++ src/pages/(main)/state/index.tsx
@@ -1,9 +1,26 @@
 import { Heading, Stack } from "@chakra-ui/react";
+import Boag from "@/components/boag"; // Assuming the correct import path for the boag component
+import { wrap } from "framer-motion";
 
 const State = () => {
   return (
-    <Stack bgColor={"red.400"} minH={"100vh"}>
-      <Heading>STATE PUNYA DIMAS</Heading>
+    <Stack
+      bgImage={"/bg/waves.png"}
+      objectFit={"fit"}
+      minH={"100vh"}
+      w={"100%"}
+    >
+      <Heading>STATE masih nunggu</Heading>
+      <Stack
+        direction={"row"}
+        px={"12rem"}
+        flexWrap={"wrap"}
+        justify={"space-evenly"}
+      >
+        <Boag />
+        <Boag />
+        <Boag />
+      </Stack>
     </Stack>
   );
 };
