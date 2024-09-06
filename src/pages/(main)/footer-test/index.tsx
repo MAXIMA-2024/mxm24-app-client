80e67e8c94264b471c8457ffbc5c4c24f6b6de7b
michael tio <tio.michael2004@gmail.com>
2024-09-06 23:00:44 +0700

    kelar responsive
    responsif belom rapih, bgImage kendala, pages align left kendala, contact dan pages ada kendala karna ga sesuai figma

--- src/pages/(main)/footer-test/index.tsx
+++ src/pages/(main)/footer-test/index.tsx
@@ -10,31 +10,30 @@ function Footer() {
         bgRepeat={"no-repeat"}
         bgSize={"cover"}
         bgPosition={"center"}
-        paddingY={5}
       >
         <Flex
           direction="row"
           justify="space-around"
           align="left"
           wrap="wrap"
-          mb={36}
+          mb={24}
         >
           {/* sponsors */}
           <Stack textAlign={"center"}>
             <Heading
               as="h3"
-              size="xl"
+              size={{ base: "lg", lg: "xl" }}
               fontFamily={"Lexend"}
               fontWeight={"semibold"}
               textColor={"#44002B"}
-              mb={6}
+              mb={{ base: 2, md: 4, lg: 6 }}
             >
               Sponsored By
             </Heading>
             <Image
               src="/footer/sponsor.png"
               alt="maxima-sponsors"
-              width={{ base: "350px", md: "500px", lg: "700px" }}
+              width={{ base: "370px", md: "500px", lg: "700px" }}
             />
           </Stack>
 
@@ -42,31 +41,31 @@ function Footer() {
           <Stack textAlign={"center"}>
             <Heading
               as="h3"
-              size="xl"
+              size={{ base: "lg", lg: "xl" }}
               fontFamily={"Lexend"}
               fontWeight={"semibold"}
               textColor={"#44002B"}
-              mb={6}
+              mb={{ base: 2, md: 4, lg: 6 }}
             >
               Media Partners
             </Heading>
             <Image
               src="/footer/medpar.png"
               alt="maxima-media-partners"
-              width={{ base: "350px", md: "500px", lg: "700px" }}
+              width={{ base: "370px", md: "500px", lg: "700px" }}
             />
           </Stack>
         </Flex>
 
-        {/* maxima */}
-        <Flex justify="space-around" align="center" w="100vw">
+        {/* maxima logo & contact*/}
+        <Flex justify="space-around" align="center" w="100vw" wrap="wrap">
           {/* maxima logo */}
           <Stack align={"start"}>
             <Image
               src="/myicons/maxima2024-logo.png"
-              width="350px"
+              width={{ base: "200px", md: "300px", lg: "350px" }}
               height={"auto"}
-              mb={4}
+              mb={{ base: 1, md: 2, lg: 4 }}
             />
             <Text
               fontFamily={"Lexend"}
@@ -74,36 +73,40 @@ function Footer() {
               textAlign={"left"}
               size={"xl"}
               textColor={"#44002B"}
-              mb={8}
+              mb={{ base: 1, md: 3, lg: 8 }}
             >
               Express Your Uniqueness, Show It To Shine
             </Text>
-            <Flex justify="start" align="left" gap={8}>
-              <Image src="/footer/logo-line.svg" width="50px" height={"auto"} />
+            <Flex justify="start" align="left" gap={8} mb={{ base: 10, lg: 0 }}>
+              <Image
+                src="/footer/logo-line.svg"
+                width={{ base: "30px", md: "40px", lg: "50px" }}
+                height={"auto"}
+              />
               <Image
                 src="/footer/logo-instagram.svg"
-                width="50px"
+                width={{ base: "30px", md: "40px", lg: "50px" }}
                 height={"auto"}
               />
               <Image
                 src="/footer/logo-tiktok.svg"
-                width="50px"
+                width={{ base: "30px", md: "40px", lg: "50px" }}
                 height={"auto"}
               />
               <Image
                 src="/footer/logo-youtube.svg"
-                width="50px"
+                width={{ base: "30px", md: "40px", lg: "50px" }}
                 height={"auto"}
               />
             </Flex>
           </Stack>
 
           {/* maxima contact & page */}
-          <Flex justify="center" gap={16}>
+          <Flex justify="center" gap={{ base: 4, lg: 16 }} paddingX={8}>
             <Stack>
               <Heading
                 textColor={"#44002B"}
-                size={"md"}
+                size={{ base: "sm", lg: "md" }}
                 fontFamily={"Lexend"}
                 fontWeight={"semibold"}
               >
@@ -113,7 +116,7 @@ function Footer() {
                 as="a"
                 href="/home"
                 variant="link"
-                size={"md"}
+                size={{ base: "sm", lg: "md" }}
                 fontFamily="Lexend"
                 textAlign="left"
                 color="black"
@@ -126,21 +129,20 @@ function Footer() {
               <Button
                 as="a"
                 href="/state"
-                size={"md"}
+                size={{ base: "sm", lg: "md" }}
                 variant="link"
                 fontFamily="Lexend"
-                textAlign="left"
                 color="black"
                 display="block"
                 width="100%"
-                fontWeight={"regular"}
+                fontWeight="regular"
               >
-                STATE
+                <Text textAlign="left">State</Text>
               </Button>
               <Button
                 as="a"
                 href="/malpun"
-                size={"md"}
+                size={{ base: "sm", lg: "md" }}
                 variant="link"
                 fontFamily="Lexend"
                 textAlign="left"
@@ -154,7 +156,7 @@ function Footer() {
               <Button
                 as="a"
                 href="/faq"
-                size={"md"}
+                size={{ base: "sm", lg: "md" }}
                 variant="link"
                 fontFamily="Lexend"
                 textAlign="left"
@@ -168,7 +170,7 @@ function Footer() {
               <Button
                 as="a"
                 href="/aboutus"
-                size={"md"}
+                size={{ base: "sm", lg: "md" }}
                 variant="link"
                 fontFamily="Lexend"
                 textAlign="left"
@@ -183,7 +185,7 @@ function Footer() {
                 as="a"
                 href="/profile"
                 variant="link"
-                size={"md"}
+                size={{ base: "sm", lg: "md" }}
                 fontFamily="Lexend"
                 textAlign="left"
                 color="black"
@@ -198,7 +200,7 @@ function Footer() {
             <Stack maxW="400px">
               <Heading
                 textColor={"#44002B"}
-                size={"md"}
+                size={{ base: "sm", lg: "md" }}
                 fontFamily={"Lexend"}
                 fontWeight={"semibold"}
               >
@@ -206,7 +208,7 @@ function Footer() {
               </Heading>
               <Text
                 fontFamily={"Lexend"}
-                size={"md"}
+                size={{ base: "sm", lg: "md" }}
                 fontWeight={"regular"}
                 minW="200px"
               >
