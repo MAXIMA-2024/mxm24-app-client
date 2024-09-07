297f8f27ffd7683d0a41e786991457b973dc1dc6
michael tio <tio.michael2004@gmail.com>
2024-09-07 21:37:01 +0700

    masih kendala background, albert memasak dulu

--- src/pages/(main)/footer-test/index.tsx
+++ src/pages/(main)/footer-test/index.tsx
@@ -5,59 +5,49 @@ function Footer() {
   return (
     <footer>
       {/* top and bot wrapper */}
-      <Stack minW={"100vw"} minH={"100vh"} gap={0}>
+      <Stack
+        minW={"100vw"}
+        minH={"100vh"}
+        gap={0}
+        fontFamily={"Lexend"}
+        color={"#44002B"}
+      >
         {/* Background Wrapper untuk top section */}
         <Stack
-        // bgImage={{
-        //   base: "/footer/footer-maxima-mobile-top.png",
-        //   lg: "/footer/footer-maxima-desktop-top.png",
-        // }}
-        // bgSize={"cover"}
-        // bgPosition={{ base: "top", lg: "center" }}
-        // bgRepeat={"no-repeat"}
-        // height="50%"
+          bgImage={{
+            base: "/footer/footer-maxima-mobile-top.png",
+            lg: "/footer/footer-maxima-desktop-top.png",
+          }}
+          bgSize={"cover"}
+          bgRepeat={"no-repeat"}
+          height={"50vh"}
+          width={"100vw"}
         >
           {/* sponsors and media partners content */}
           <Flex
             direction={{ base: "column", lg: "row" }}
-            justify="space-around"
             align={"center"}
+            justify={"space-around"}
           >
             {/* sponsors */}
-            <Stack textAlign={"center"}>
-              <Heading
-                as="h3"
-                size={{ base: "lg", lg: "xl" }}
-                fontFamily={"Lexend"}
-                fontWeight={"semibold"}
-                textColor={"#44002B"}
-                mb={{ base: 2, md: 4, lg: 6 }}
-              >
+            <Stack>
+              <Heading textAlign={"center"} fontWeight={"semibold"}>
                 Sponsored By
               </Heading>
               <Image
                 src="/footer/sponsor.png"
-                alt="maxima-sponsors"
-                width={{ base: "370px", md: "500px", lg: "700px" }}
+                width={{ base: "500px", md: "520px", lg: "700px" }}
               />
             </Stack>
 
             {/* media partners */}
-            <Stack textAlign={"center"}>
-              <Heading
-                as="h3"
-                size={{ base: "lg", lg: "xl" }}
-                fontFamily={"Lexend"}
-                fontWeight={"semibold"}
-                textColor={"#44002B"}
-                mb={{ base: 2, md: 4, lg: 6 }}
-              >
+            <Stack>
+              <Heading textAlign={"center"} fontWeight={"semibold"}>
                 Media Partners
               </Heading>
               <Image
                 src="/footer/medpar.png"
-                alt="maxima-media-partners"
-                width={{ base: "370px", md: "500px", lg: "700px" }}
+                width={{ base: "500px", md: "520px", lg: "700px" }}
               />
             </Stack>
           </Flex>
@@ -65,47 +55,30 @@ function Footer() {
 
         {/* Background Wrapper untuk bottom section */}
         <Stack
-        // bgImage={{
-        //   base: "/footer/footer-maxima-mobile-bot.png",
-        //   lg: "/footer/footer-maxima-desktop-bot.png",
-        // }}
-        // bgSize={"cover"}
-        // bgRepeat={"no-repeat"}
-        // bgPosition={{ base: "top", lg: "center" }}
-        // height="50%"
+          bgImage={{
+            base: "/footer/footer-maxima-mobile-bot.png",
+            lg: "/footer/footer-maxima-desktop-bot.png",
+          }}
+          bgSize={"cover"}
+          bgRepeat={"no-repeat"}
+          height={"50vh"}
+          width={"100vw"}
         >
           {/* maxima logo & contact*/}
           <Flex
-            justify={{ base: "start", lg: "space-between" }}
             direction={{ base: "column", lg: "row" }}
-            align={{ base: "", md: "center" }}
-            paddingX={{ base: 8, lg: 32 }}
-            width={"full"}
+            justify={"space-between"}
+            align={{ base: "start", sm: "center" }}
+            paddingX={{ base: 4, lg: 28 }}
           >
             {/* maxima logo */}
-            <Stack align={"start"}>
+            <Stack marginBottom={{ base: 10, lg: 0 }}>
               <Image
                 src="/myicons/maxima2024-logo.png"
-                width={{ base: "200px", md: "300px", lg: "350px" }}
-                height={"auto"}
-                mb={{ base: 1, md: 2, lg: 4 }}
+                width={{ base: "150px", md: "200px", lg: "300px" }}
               />
-              <Text
-                fontFamily={"Lexend"}
-                fontWeight={"regular"}
-                textAlign={"left"}
-                size={"xl"}
-                textColor={"#44002B"}
-                mb={{ base: 1, md: 3, lg: 8 }}
-              >
-                Express Your Uniqueness, Show It To Shine
-              </Text>
-              <Flex
-                justify="start"
-                align="left"
-                gap={8}
-                mb={{ base: 10, lg: 0 }}
-              >
+              <Text>Express Your Uniqueness, Show It To Shine</Text>
+              <Flex>
                 <Link href="https://line.me/ti/p/vuu4204x" isExternal>
                   <Image
                     src="/footer/logo-line.svg"
@@ -138,98 +111,32 @@ function Footer() {
             </Stack>
 
             {/* maxima contact & page */}
-            <Flex justify="center" gap={{ base: 4, lg: 16 }} paddingBottom={8}>
+            <Flex gap={{ base: 4, lg: 16 }} paddingBottom={8}>
               <Stack>
-                <Heading
-                  textColor={"#44002B"}
-                  size={{ base: "sm", lg: "md" }}
-                  fontFamily={"Lexend"}
-                  fontWeight={"semibold"}
-                >
-                  Pages
-                </Heading>
-                <Link
-                  as={RouterLink}
-                  to={"/home"}
-                  size={{ base: "sm", lg: "md" }}
-                  fontFamily="Lexend"
-                  textAlign="left"
-                  color="black"
-                  fontWeight={"regular"}
-                >
+                <Heading size={{ base: "sm", lg: "md" }}>Pages</Heading>
+                <Link as={RouterLink} to={"/home"} color={"black"}>
                   HoME
                 </Link>
-                <Link
-                  as={RouterLink}
-                  to={"/state"}
-                  size={{ base: "sm", lg: "md" }}
-                  fontFamily="Lexend"
-                  textAlign="left"
-                  color="black"
-                  fontWeight={"regular"}
-                >
+                <Link as={RouterLink} to={"/state"} color={"black"}>
                   STATE
                 </Link>
-                <Link
-                  as={RouterLink}
-                  to={"/malpun"}
-                  size={{ base: "sm", lg: "md" }}
-                  fontFamily="Lexend"
-                  textAlign="left"
-                  color="black"
-                  fontWeight={"regular"}
-                >
+                <Link as={RouterLink} to={"/malpun"} color={"black"}>
                   MalPun
                 </Link>
-                <Link
-                  as={RouterLink}
-                  to={"/faq"}
-                  size={{ base: "sm", lg: "md" }}
-                  fontFamily="Lexend"
-                  textAlign="left"
-                  color="black"
-                  fontWeight={"regular"}
-                >
+                <Link as={RouterLink} to={"/faq"} color={"black"}>
                   FAQ
                 </Link>
-                <Link
-                  as={RouterLink}
-                  to={"/aboutus"}
-                  size={{ base: "sm", lg: "md" }}
-                  fontFamily="Lexend"
-                  textAlign="left"
-                  color="black"
-                  fontWeight={"regular"}
-                >
+                <Link as={RouterLink} to={"/aboutus"} color={"black"}>
                   About Us
                 </Link>
-                <Link
-                  as={RouterLink}
-                  to={"/profile"}
-                  size={{ base: "sm", lg: "md" }}
-                  fontFamily="Lexend"
-                  textAlign="left"
-                  color="black"
-                  fontWeight={"regular"}
-                >
+                <Link as={RouterLink} to={"/profile"} color={"black"}>
                   Profile
                 </Link>
               </Stack>
 
               <Stack maxW="270px">
-                <Heading
-                  textColor={"#44002B"}
-                  size={{ base: "sm", lg: "md" }}
-                  fontFamily={"Lexend"}
-                  fontWeight={"semibold"}
-                >
-                  Location
-                </Heading>
-                <Text
-                  fontFamily={"Lexend"}
-                  size={{ base: "sm", lg: "md" }}
-                  fontWeight={"regular"}
-                >
+                <Heading size={{ base: "sm", lg: "md" }}>Location</Heading>
+                <Text size={{ base: "sm", lg: "md" }} color={"black"}>
                   Universitas Multimedia Nusantara Jl. Scientia Boulevard,
                   Gading Serpong, Tangerang, Banten 15811
                 </Text>
@@ -239,8 +146,13 @@ function Footer() {
         </Stack>
         {/* Footer Copyright */}
       </Stack>
-      <Stack bg={"#44002B"} textAlign={"center"} paddingY={3}>
-        <Text color={"white"} fontFamily={"Lexend"} fontWeight={"regular"}>
+      <Stack
+        bg={"#44002B"}
+        paddingY={3}
+        fontFamily={"Lexend"}
+        fontWeight={"regular"}
+      >
+        <Text color={"white"} textAlign={"center"}>
           © MAXIMA UMN 2024 • Made with Love by Charta
         </Text>
       </Stack>
